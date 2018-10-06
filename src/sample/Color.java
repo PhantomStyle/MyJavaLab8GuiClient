6974e586b7da8bef1998e5e559436d04c3d7ffbe
pro10 <pro100sto18@gmail.com>
2018-10-06 19:17:18 +0300

    Добавил квадратики к сообщениям

---
+++ src/sample/Color.java
@@ -0,0 +1,5 @@
+package sample;
+
+public enum  Color {
+    Black, Blue, Green, Red
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,8 +7,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -20,23 +22,66 @@ import java.util.Map;
 
 public class Main extends Application {
 
-    @FXML
-    private Button button;
 
     @FXML
     private ListView<String> showingField;
 
     @FXML
-    private PasswordField passField;
+    private Rectangle r2;
 
     @FXML
-    private TextField enteringField;
+    private Rectangle r3;
+
+    @FXML
+    private Rectangle r4;
+
+    @FXML
+    private Rectangle r5;
 
     @FXML
     private Button buttonLogin;
 
     @FXML
-    private TextField loginField;
+    private Rectangle r6;
+
+    @FXML
+    private Rectangle r7;
+
+    @FXML
+    private Rectangle r8;
+
+    @FXML
+    private Rectangle r9;
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Rectangle r10;
+
+    @FXML
+    private PasswordField passField;
+
+    @FXML
+    private Rectangle r12;
+
+    @FXML
+    private Rectangle r11;
+
+    @FXML
+    private TextField enteringField;
+
+    @FXML
+    private Button redB;
+
+    @FXML
+    private Button greenB;
+
+    @FXML
+    private CheckBox check;
+
+    @FXML
+    private BarChart<?, ?> gist;
 
     @FXML
     private Text text1;
@@ -44,13 +89,34 @@ public class Main extends Application {
     @FXML
     private Text text2;
 
+    @FXML
+    private Button blackB;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private Button blueB;
+
+    @FXML
+    private Rectangle r1;
+
+
     private String clientCommand = "";
+
+    private Color color;
 
 
     private static Stage stage;
 
     private String login;
     private String pass;
+
+    private static Map<Integer, Rectangle> mapOfRectangles = new HashMap<>();
+
+
+    private static Map<Integer, String> mapOfMessages = new HashMap<>();
+    private static int idOfMessage = 0;
 
     private static Map<String, String> db = new HashMap<>();
 
@@ -76,9 +142,20 @@ public class Main extends Application {
                 buttonLogin.setVisible(false);
                 text1.setVisible(false);
                 text2.setVisible(false);
+//                greenB.setVisible(true);
+//                blackB.setVisible(true);
+//                blueB.setVisible(true);
+//                redB.setVisible(true);
+                check.setVisible(false);
                 button.setVisible(true);
                 showingField.setVisible(true);
                 enteringField.setVisible(true);
+
+                greenB.setVisible(true);
+                blueB.setVisible(true);
+                blackB.setVisible(true);
+                redB.setVisible(true);
+
                 name = login;
             }
         } catch (Exception e) {
@@ -109,6 +186,43 @@ public class Main extends Application {
         button.setVisible(false);
         showingField.setVisible(false);
         enteringField.setVisible(false);
+//        greenB.setVisible(false);
+//        blackB.setVisible(false);
+//        blueB.setVisible(false);
+//        redB.setVisible(false);
+        check.setVisible(false);
+        gist.setVisible(false);
+        r1.setVisible(false);
+        r2.setVisible(false);
+        r3.setVisible(false);
+        r4.setVisible(false);
+        r5.setVisible(false);
+        r6.setVisible(false);
+        r7.setVisible(false);
+        r8.setVisible(false);
+        r9.setVisible(false);
+        r10.setVisible(false);
+        r11.setVisible(false);
+        r12.setVisible(false);
+
+        mapOfRectangles.put(0, r1);
+        mapOfRectangles.put(1, r2);
+        mapOfRectangles.put(2, r3);
+        mapOfRectangles.put(3, r4);
+        mapOfRectangles.put(4, r5);
+        mapOfRectangles.put(5, r6);
+        mapOfRectangles.put(6, r7);
+        mapOfRectangles.put(7, r8);
+        mapOfRectangles.put(8, r9);
+        mapOfRectangles.put(9, r10);
+        mapOfRectangles.put(10, r11);
+        mapOfRectangles.put(11, r12);
+
+        greenB.setVisible(false);
+        blueB.setVisible(false);
+        blackB.setVisible(false);
+        redB.setVisible(false);
+
         Socket socket = new Socket("localhost", 3345);
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        DataOutputStream oos = new DataOutputStream(socket.getOutputStream());
@@ -216,7 +330,13 @@ public class Main extends Application {
                         String in = reader.readLine();
                         System.out.println("After reading");
                         //                        String in = ois.readUTF();
-                            showingField.getItems().add(in);
+                        mapOfMessages.put(idOfMessage, "        " + in);
+                        showingField.getItems().add("        " + in);
+                        mapOfRectangles.get(idOfMessage).setVisible(true);
+                        if(!in.split(":")[0].contains(name)){
+                            mapOfRectangles.get(idOfMessage).setFill(javafx.scene.paint.Color.BROWN);
+                        }
+                        idOfMessage++;
 //                            System.out.println(in);
 
 //                        }
@@ -242,6 +362,107 @@ public class Main extends Application {
         clientCommand = enteringField.getText();
         enteringField.clear();
 
+    }
+
+    @FXML
+    private void onCheckAction() {
+        check.setVisible(true);
+    }
+
+    @FXML
+    private void onBlue() {
+        for (Map.Entry<Integer, String> entry : mapOfMessages.entrySet()) {
+            if (!(entry.getValue().split(":")[0].contains(name))) {
+                if (mapOfRectangles.get(entry.getKey()).getFill().equals(javafx.scene.paint.Color.BLUE)) {
+                    Stage st = new Stage();
+                    st.initModality(Modality.APPLICATION_MODAL);
+                    st.initOwner(stage);
+                    VBox dialogVbox = new VBox(20);
+                    dialogVbox.getChildren().add(new Text("Color is used"));
+                    Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                    st.setScene(dialogScene);
+                    st.show();
+                }
+            }
+        }
+        for (Map.Entry<Integer, String> entry : mapOfMessages.entrySet()) {
+            if (entry.getValue().split(":")[0].contains(name)) {
+                mapOfRectangles.get(entry.getKey()).setFill(javafx.scene.paint.Color.BLUE);
+                mapOfRectangles.get(entry.getKey()).setVisible(true);
+            }
+        }
+    }
+
+    @FXML
+    private void onRed() {
+        for (Map.Entry<Integer, String> entry : mapOfMessages.entrySet()) {
+            if (!(entry.getValue().split(":")[0].contains(name))) {
+                if (mapOfRectangles.get(entry.getKey()).getFill().equals(javafx.scene.paint.Color.RED)) {
+                    Stage st = new Stage();
+                    st.initModality(Modality.APPLICATION_MODAL);
+                    st.initOwner(stage);
+                    VBox dialogVbox = new VBox(20);
+                    dialogVbox.getChildren().add(new Text("Color is used"));
+                    Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                    st.setScene(dialogScene);
+                    st.show();
+                }
+            }
+        }
+        for (Map.Entry<Integer, String> entry : mapOfMessages.entrySet()) {
+            if (entry.getValue().split(":")[0].contains(name)) {
+                mapOfRectangles.get(entry.getKey()).setFill(javafx.scene.paint.Color.RED);
+                mapOfRectangles.get(entry.getKey()).setVisible(true);
+            }
+        }
+    }
+
+    @FXML
+    private void onBlack() {
+        for (Map.Entry<Integer, String> entry : mapOfMessages.entrySet()) {
+            if (!(entry.getValue().split(":")[0].contains(name))) {
+                if (mapOfRectangles.get(entry.getKey()).getFill().equals(javafx.scene.paint.Color.BLACK)) {
+                    Stage st = new Stage();
+                    st.initModality(Modality.APPLICATION_MODAL);
+                    st.initOwner(stage);
+                    VBox dialogVbox = new VBox(20);
+                    dialogVbox.getChildren().add(new Text("Color is used"));
+                    Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                    st.setScene(dialogScene);
+                    st.show();
+                }
+            }
+        }
+        for (Map.Entry<Integer, String> entry : mapOfMessages.entrySet()) {
+            if (entry.getValue().split(":")[0].contains(name)) {
+                mapOfRectangles.get(entry.getKey()).setFill(javafx.scene.paint.Color.BLACK);
+                mapOfRectangles.get(entry.getKey()).setVisible(true);
+            }
+        }
+    }
+
+    @FXML
+    private void onGreen() {
+        for (Map.Entry<Integer, String> entry : mapOfMessages.entrySet()) {
+            if (!(entry.getValue().split(":")[0].contains(name))) {
+                if (mapOfRectangles.get(entry.getKey()).getFill().equals(javafx.scene.paint.Color.GREEN)) {
+                    Stage st = new Stage();
+                    st.initModality(Modality.APPLICATION_MODAL);
+                    st.initOwner(stage);
+                    VBox dialogVbox = new VBox(20);
+                    dialogVbox.getChildren().add(new Text("Color is used"));
+                    Scene dialogScene = new Scene(dialogVbox, 300, 200);
+                    st.setScene(dialogScene);
+                    st.show();
+                }
+            }
+        }
+        for (Map.Entry<Integer, String> entry : mapOfMessages.entrySet()) {
+            if (entry.getValue().split(":")[0].contains(name)) {
+                mapOfRectangles.get(entry.getKey()).setFill(javafx.scene.paint.Color.GREEN);
+                mapOfRectangles.get(entry.getKey()).setVisible(true);
+            }
+        }
     }
 
     public static void main(String[] args) {
